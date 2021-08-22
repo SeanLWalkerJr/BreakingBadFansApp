2c47c7b66f3cc7f669856c936e3a95fda56309fb
Sean Walker <seanleewalkerjr@gmail.com>
2021-08-22 12:51:42 -0400

    Added EpisodesViewModel

---
+++ BreakingBadFansApp/ViewModels/EpisodesViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  CharacterViewModel.swift
+//  BreakingBadFansApp
+//
+//
+
+import Foundation
+
+class EpisodesViewModel : ObservableObject
+{
+    @Published var episodes = [Episode]()
+    
+    func fetchEpisodes()
+    {
+        let url = URL(string: "https://www.breakingbadapi.com/api/episodes")!
+        URLSession.shared.dataTask(with: url) {data, _, error in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                
+                if let data = data {
+                    let decoder = JSONDecoder()
+                    do {
+                        let episodes = try decoder.decode([Episode].self, from: data)
+                        DispatchQueue.main.async {
+                            self.episodes = episodes
+                        }
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+        }.resume()
+    }
+}
---
+++ BreakingBadFansApp/Views/EpisodesView.swift
@@ -0,0 +1,17 @@
+//
+//  EpisodesView.swift
+//  BreakingBadFansApp
+//
+//  Created by User 1 on 8/22/21.
+//
+
+import SwiftUI
+
+struct EpisodesView: View {
+    var body: some View {
+        NavigationView
+        {
+            
+        }
+    }
+}
