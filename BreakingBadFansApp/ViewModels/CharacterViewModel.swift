d7acee02396ff716b03fd7e050a1f252232b64ce
Sean Walker <seanleewalkerjr@gmail.com>
2021-08-22 10:46:13 -0400

    Added character view model

---
+++ BreakingBadFansApp/ViewModels/CharacterViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  CharacterViewModel.swift
+//  BreakingBadFansApp
+//
+//
+
+import Foundation
+
+class CharacterViewModel : ObservableObject
+{
+    @Published var characters = [Character]()
+    
+    func fetchCharacters()
+    {
+        let url = URL(string: "https://www.breakingbadapi.com/api/")!
+        URLSession.shared.dataTask(with: url) {data, _, error in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                
+                if let data = data {
+                    let decoder = JSONDecoder()
+                    do {
+                        let characters = try decoder.decode([Character].self, from: data)
+                        DispatchQueue.main.async {
+                            self.characters = characters
+                        }
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+        }.resume()
+    }
+}
