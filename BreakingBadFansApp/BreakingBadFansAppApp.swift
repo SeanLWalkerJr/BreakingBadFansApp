b454fae64a0bff3af286aaf5061b1437ffb4d3f8
Sean Walker <seanleewalkerjr@gmail.com>
2021-08-21 21:58:27 -0400

    Initialized repo and added all the files

---
+++ BreakingBadFansApp/BreakingBadFansAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadFansAppApp.swift
+//  BreakingBadFansApp
+//
+//  Created by User 1 on 8/21/21.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadFansAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreakingBadFansApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BreakingBadFansApp
+//
+//  Created by User 1 on 8/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
