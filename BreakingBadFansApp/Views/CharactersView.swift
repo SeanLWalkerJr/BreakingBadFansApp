ee99532c8cc10d5dedafe040524e57d51c269234
Sean Walker <seanleewalkerjr@gmail.com>
2021-08-22 12:25:05 -0400

    Added nicknames under character images on CharactersView

--- BreakingBadFansApp/Views/CharactersView.swift
+++ BreakingBadFansApp/Views/CharactersView.swift
@@ -12,26 +12,30 @@ struct CharactersView: View {
         GridItem(.flexible(minimum: 100, maximum: 200), spacing: 16)
     ]
     
-    
     var body: some View {
         NavigationView{
             ScrollView{
-                LazyVGrid(columns: columns, content: {
-                    ForEach(characterViewModel.characters) { character in
-                        
-                        NavigationLink(destination: CharacterDetailView(character: character), label: {
-                            WebImage(url: character.img)
-                                .resizable()
-                                .indicator(.activity)
-                                .aspectRatio(contentMode: .fit)
-                                .frame(height: 150)
-                                .cornerRadius(8)
-                            
-                        })
-                        
-                    }
-                })
-                .padding()
+                LazyVGrid(columns: columns, content:
+                            {
+                                ForEach(characterViewModel.characters)
+                                { character in
+                                    NavigationLink(destination: CharacterDetailView(character: character), label:
+                                                    {
+                                                        VStack(alignment: .center)
+                                                        {
+                                                            WebImage(url: character.img)
+                                                                .resizable()
+                                                                .indicator(.activity)
+                                                                .aspectRatio(contentMode: .fit)
+                                                                .frame(height: 150)
+                                                                .cornerRadius(8)
+                                                            Text(character.nickname)
+                                                        }
+                                                        
+                                                    })
+                                }
+                            })
+                    .padding()
             }
             .navigationTitle("Characters")
             .onAppear(perform: {
